54ec05c4b1063680d067f88bb140993df3cbf87c
fulgurance <zohran.londais@gmail.com>
2023-02-21 13:47:51 +0000

    Updated Gzip, Tar and Xz to support Pass option

--- Tar/1.34.0/1.34.0.cr
+++ Tar/1.34.0/1.34.0.cr
@@ -2,21 +2,35 @@ class Target < ISM::Software
     
     def configure
         super
-        configureSource([   "--prefix=/usr"],
-                            buildDirectoryPath,
-                            "",
-                            {"FORCE_UNSAFE_CONFIGURE" => "1"})
+
+        if option("Pass1")
+            configureSource([   "--prefix=#{Ism.settings.rootPath}usr",
+                            "--host=#{Ism.settings.target}",
+                            "--build=$(build-aux/config.guess)"],
+                            buildDirectoryPath)
+        else
+            configureSource([   "--prefix=/usr"],
+                                buildDirectoryPath,
+                                "",
+                                {"FORCE_UNSAFE_CONFIGURE" => "1"})
+        end
     end
     
     def build
         super
+
         makeSource([Ism.settings.makeOptions],buildDirectoryPath)
     end
     
     def prepareInstallation
         super
-        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
-        makeSource([Ism.settings.makeOptions,"-C","doc","DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install-html","docdir=/usr/share/doc/tar-1.34"],buildDirectoryPath)
+
+        if option("Pass1")
+            makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}","install"],buildDirectoryPath)
+        else
+            makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+            makeSource([Ism.settings.makeOptions,"-C","doc","DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install-html","docdir=/usr/share/doc/tar-1.34"],buildDirectoryPath)
+        end
     end
 
 end
--- Xz/5.2.5/5.2.5.cr
+++ Xz/5.2.5/5.2.5.cr
@@ -2,20 +2,36 @@ class Target < ISM::Software
 
     def configure
         super
-        configureSource([   "--prefix=/usr",
-                            "--disable-static",
-                            "--docdir=/usr/share/doc/xz-5.2.5"],
-                            buildDirectoryPath)
+
+        if option("Pass1")
+            configureSource([   "--prefix=#{Ism.settings.rootPath}/usr",
+                                "--host=#{Ism.settings.target}",
+                                "--build=$(build-aux/config.guess)",
+                                "--disable-static",
+                                "--docdir=#{Ism.settings.rootPath}/usr/share/doc/xz-5.2.5"],
+                                buildDirectoryPath)
+        else
+            configureSource([   "--prefix=/usr",
+                                "--disable-static",
+                                "--docdir=/usr/share/doc/xz-5.2.5"],
+                                buildDirectoryPath)
+        end
     end
 
     def build
         super
+
         makeSource([Ism.settings.makeOptions],buildDirectoryPath)
     end
 
     def prepareInstallation
         super
-        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+
+        if option("Pass1")
+            makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}","install"],buildDirectoryPath)
+        else
+            makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+        end
     end
 
 end
