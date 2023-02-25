cb15fadbb8410757c5018a3e4ed20defdc3a6b7e
fulgurance <zohran.londais@gmail.com>
2023-02-25 10:12:51 +0000

    Patched symlinks errors

--- Bzip2/1.0.8/1.0.8.cr
+++ Bzip2/1.0.8/1.0.8.cr
@@ -2,6 +2,7 @@ class Target < ISM::Software
 
     def prepare
         super
+
         fileReplaceText("#{buildDirectoryPath(false)}/Makefile","ln -s -f $(PREFIX)/bin/","ln -s -f ")
         fileReplaceText("#{buildDirectoryPath(false)}/Makefile","$(PREFIX)/man","$(PREFIX)/share/man")
         makeSource([Ism.settings.makeOptions,"-f","Makefile-libbz2_so"],buildDirectoryPath)
@@ -10,11 +11,13 @@ class Target < ISM::Software
 
     def build
         super
+
         makeSource([Ism.settings.makeOptions],buildDirectoryPath)
     end
 
     def prepareInstallation
         super
+
         makeSource([Ism.settings.makeOptions,"PREFIX=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}/usr","install"],buildDirectoryPath)
         copyFile(Dir["#{buildDirectoryPath(false)}/libbz2.so.*"],"#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/lib")
         copyFile("#{buildDirectoryPath(false)}/bzip2-shared","#{builtSoftwareDirectoryPath(false)}/#{Ism.settings.rootPath}/usr/bin/bzip2")
@@ -22,6 +25,7 @@ class Target < ISM::Software
 
     def install
         super
+
         makeLink("libbz2.so.1.0.8","#{Ism.settings.rootPath}/usr/lib/libbz2.so",:symbolicLink)
         makeLink("bzip2","#{Ism.settings.rootPath}/usr/bin/bzcat",:symbolicLinkByOverwrite)
         makeLink("bzip2","#{Ism.settings.rootPath}/usr/bin/bunzip2",:symbolicLinkByOverwrite)
@@ -29,6 +33,7 @@ class Target < ISM::Software
 
     def clean
         super
+
         deleteFile("#{Ism.settings.rootPath}/usr/lib/libbz2.a")
     end
 
