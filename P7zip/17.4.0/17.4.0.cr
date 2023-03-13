59b03c9b960cb5f07e06239280a07e4916968611
fulgurance <zohran.londais@gmail.com>
2023-03-13 22:44:29 +0000

    Added P7zip installer

---
+++ P7zip/17.4.0/17.4.0.cr
@@ -0,0 +1,24 @@
+class Target < ISM::Software
+
+    def prepare
+        super
+
+        fileDeleteLine("#{buildDirectoryPath(false)}install.sh",162)
+        fileDeleteLine("#{buildDirectoryPath(false)}install.sh",162)
+        fileDeleteLine("#{buildDirectoryPath(false)}install.sh",162)
+        fileReplaceTextAtLineNumber("#{buildDirectoryPath(false)}CPP/7zip/Common/StreamObjects.cpp", "return E_FAIL;","if(_buffer == nullptr || _size == _pos) return E_FAIL;\nreturn E_FAIL;",161)
+    end
+
+    def build
+        super
+
+        makeSource([Ism.settings.makeOptions,"all3"],buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        makeSource([Ism.settings.makeOptions,"DEST_DIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}","DEST_HOME=/usr","DEST_MAN=/usr/share/man","DEST_SHARE_DOC=/usr/share/doc/p7zip-17.04","install"],buildDirectoryPath)
+    end
+
+end
