a872b7867b7c055b6aeb53e1124477d5c114a382
Fulgurance <zohran.londais@gmail.com>
2024-07-05 19:39:07 +0100

    Removed 32bits support

--- Xz/5.4.4/5.4.4.cr
+++ Xz/5.4.4/5.4.4.cr
@@ -1,22 +1,5 @@
 class Target < ISM::Software
 
-    def prepare
-        if !option("Pass1")
-            if option("32Bits") || option("x32Bits")
-                @buildDirectory = true
-            end
-
-            if option("32Bits")
-                @buildDirectoryNames["32Bits"] = "mainBuild-32"
-            end
-
-            if option("x32Bits")
-                @buildDirectoryNames["x32Bits"] = "mainBuild-x32"
-            end
-        end
-        super
-    end
-
     def configure
         super
 
@@ -32,23 +15,6 @@ class Target < ISM::Software
                                         --disable-static    \
                                         --docdir=/usr/share/doc/xz-5.4.4",
                             path:       buildDirectoryPath)
-
-            if option("32Bits")
-                configureSource(arguments:      "--host=i686-#{Ism.settings.systemTargetName}-linux-gnu \
-                                                --prefix=/usr                                           \
-                                                --libdir=/usr/lib32                                     \
-                                                --disable-static",
-                                path:           buildDirectoryPath(entry: "32Bits"),
-                                environment:    {"CC" =>"gcc -m32"})
-            end
-
-            if option("x32Bits")
-                configureSource(arguments:      "--host=#{Ism.settings.systemTarget}x32 \
-                                                --prefix=/usr \--libdir=/usr/libx32     \
-                                                --disable-static",
-                                path:           buildDirectoryPath(entry: "x32Bits"),
-                                environment:    {"CC" =>"gcc -mx32"})
-            end
         end
     end
 
@@ -56,16 +22,6 @@ class Target < ISM::Software
         super
 
         makeSource(path: buildDirectoryPath)
-
-        if !option("Pass1")
-            if option("32Bits")
-                makeSource(path: buildDirectoryPath(entry: "32Bits"))
-            end
-
-            if option("x32Bits")
-                makeSource(path: buildDirectoryPath(entry: "x32Bits"))
-            end
-        end
     end
 
     def prepareInstallation
@@ -76,28 +32,6 @@ class Target < ISM::Software
 
         if option("Pass1")
             deleteFile("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/lib/liblzma.la")
-        else
-            if option("32Bits")
-                makeDirectory("#{buildDirectoryPath(entry: "32Bits")}/32Bits")
-                makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr")
-
-                makeSource( arguments:   "DESTDIR=#{buildDirectoryPath(entry: "32Bits")}/32Bits install",
-                            path: buildDirectoryPath(entry: "32Bits"))
-
-                copyDirectory(  "#{buildDirectoryPath(entry: "32Bits")}/32Bits/usr/lib32",
-                                "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/lib32")
-            end
-
-            if option("x32Bits")
-                makeDirectory("#{buildDirectoryPath(entry: "x32Bits")}/x32Bits")
-                makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr")
-
-                makeSource( arguments:   "DESTDIR=#{buildDirectoryPath(entry: "x32Bits")}/x32Bits install",
-                            path: buildDirectoryPath(entry: "x32Bits"))
-
-                copyDirectory(  "#{buildDirectoryPath(entry: "x32Bits")}/x32Bits/usr/libx32",
-                                "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr/libx32")
-            end
         end
     end
 
