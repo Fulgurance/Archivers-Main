be44e19f54bc1efd3a706760d4e4bc58e232cc27
Fulgurance <zohran.londais@gmail.com>
2025-04-23 16:08:07 +0100

    Patched cross tool chain building process

--- Xz/5.6.3/5.6.3.cr
+++ Xz/5.6.3/5.6.3.cr
@@ -6,7 +6,7 @@ class Target < ISM::Software
         if option("Pass1")
             configureSource(arguments:  "--prefix=/usr                          \
                                         --host=#{Ism.settings.chrootTarget}     \
-                                        --build=#{Ism.settings.chrootTarget}    \
+                                        --build=#{Ism.settings.systemTarget}    \
                                         --disable-static                        \
                                         --disable-doc",
                             path:       buildDirectoryPath)
